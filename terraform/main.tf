58581d8e9fa641c8305703a5a39a37b44d1197cb
Joao Pedro Henrique <joao.pedro.hsd@gmail.com>
2022-12-08 13:04:40 -0300

    build: remove cloudflare configuration.
    The website now uses Vercel nameservers in order to configure DNS. So no more cloudflare config required

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,9 +1,5 @@
 terraform {
   required_providers {
-    cloudflare = {
-      source  = "cloudflare/cloudflare"
-      version = "~> 3.0"
-    }
     vercel = {
       source  = "vercel/vercel"
       version = "~> 0.3"
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,13 +1,3 @@
-variable "cloudflare_email" {
-  type        = string
-  description = "clouflare email address"
-}
-
-variable "cloudflare_api_key" {
-  type        = string
-  description = "cloudflare api key"
-}
-
 variable "vercel_api_token" {
   type        = string
   description = "vercel api token"
