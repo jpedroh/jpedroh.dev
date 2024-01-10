00044151faec19fa92517ab0d395bafacc56c10a
Joao Duarte <joao.pedro.hsd@gmail.com>
2024-01-10 20:19:52 -0300

    feat: Add background style

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,7 +16,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={`${inter.className} min-h-screen bg-gradient-to-l from-zinc-950 to-zinc-900`}>
+        {children}
+      </body>
     </html>
   )
 }
