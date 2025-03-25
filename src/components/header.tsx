3ce38a43182c530fb4ed1d986f85a48a50df44ed
Joao Duarte <joao.pedro.hsd@gmail.com>
2025-03-24 21:24:08 -0300

    feat: improve responsiveness

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -4,7 +4,7 @@ import { twc } from "react-twc";
 import { Paragraph } from "./paragraph";
 
 export const Header = {
-  Root: twc.header`flex flex-col gap-4 sticky top-0 max-h-screen py-24 justify-between h-full`,
+  Root: twc.header`flex flex-col gap-4 md:sticky top-0 max-h-screen py-12 md:py-24 justify-between h-full`,
   Title: twc.h1`text-zinc-100 font-bold text-5xl`,
   Subtitle: twc.h2`text-zinc-300 font-semibold text-2xl`,
   Paragraph: twc(Paragraph)`max-w-96`,
