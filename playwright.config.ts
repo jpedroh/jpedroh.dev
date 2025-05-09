8ef544d778b377f1debdb4c194b671ca1eada29e
Joao Duarte <joao.pedro.hsd@gmail.com>
2025-05-08 22:30:25 -0400

    test: uncomment

--- playwright.config.ts
+++ playwright.config.ts
@@ -63,9 +63,9 @@ export default defineConfig({
   ],
 
   /* Run your local dev server before starting the tests */
-  // webServer: {
-  //   command: 'pnpm preview',
-  //   url: 'http://127.0.0.1:4321',
-  //   reuseExistingServer: !process.env.CI,
-  // },
+  webServer: {
+    command: 'pnpm preview',
+    url: 'http://127.0.0.1:4321',
+    reuseExistingServer: !process.env.CI,
+  },
 });
